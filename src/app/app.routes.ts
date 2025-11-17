fbb541a307abe8a2707dbbbf1f471dcbf624bfea
Maysel <y.camarasa@gmail.com>
2025-11-17 16:11:34 +0100

    Added homepage, architecture restructuring

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
-import { GitCourseComponent } from './git-course/git-course.component';
+import { GitCourseComponent } from './pages/git-course/git-course.component';
+import { AngularCourseComponent } from './pages/angular-course/angular-course.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 export const routes: Routes = [
   {
     path: '',
+    component: HomepageComponent,
+  },
+  {
+    path: 'git',
     component: GitCourseComponent,
+  },
+  {
+    path: 'angular',
+    component: AngularCourseComponent,
   },
   {
     path: '**',
---
+++ src/app/pages/angular-course/angular-course.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-course',
+  imports: [],
+  templateUrl: './angular-course.component.html',
+  styleUrl: './angular-course.component.scss',
+})
+export class AngularCourseComponent {
+
+}
