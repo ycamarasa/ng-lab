083aba9dd97cc329be8028da21c56c85c6d153c7
Yolanda <y.camarasa@gmail.com>
2026-02-16 11:07:07 +0100

    Added new page

---
+++ src/app/pages/rxjs-course/rxjs-course.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rxjs-course',
+  imports: [],
+  templateUrl: './rxjs-course.component.html',
+  styleUrl: './rxjs-course.component.scss',
+})
+export class RxjsCourseComponent {
+
+}
